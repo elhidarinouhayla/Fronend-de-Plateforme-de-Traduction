96182d2be78c67d459f9d3d3f1d677b7a907e9c9
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-11-29 01:48:22 +0100

    js pour la page de signup

---
+++ signup/signup.js
@@ -0,0 +1,26 @@
+document.getElementById("signup-form").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch('http://127.0.0.1:8000/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ username, password })
+        });
+
+        if (!response.ok) {
+            alert("Impossible de créer le compte. Essayez un autre username.");
+            return;
+        }
+
+        // alert("Compte créé avec succès");
+        window.location.href = "../login/page_login.html";
+
+    } catch (error) {
+        console.error("Erreur:", error);
+        alert("Erreur interne du serveur");
+    }
+});
