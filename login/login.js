c65a9c22487f6645f9c2c098a7a5395f60d9824a
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-11-29 01:47:17 +0100

    js pour la page de login

---
+++ login/login.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form')
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+    const ErrorElement = document.getElementById('error')
+
+    form,addEventListener( 'submit', async (e) =>{
+        e.preventDefault();
+        console.log(password )
+        try {
+            const response = await fetch('http://127.0.0.1:8000/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({ username, password })
+            });
+
+            console.log(response)
+
+            if (!response.ok) {
+                alert("Username ou Password incorrect");
+                return;
+            }
+
+            const data = await response.json();
+            console.log(data)
+            localStorage.setItem("token", data.token);
+
+            // redirection vers translate
+            window.location.href = "../translate/page_translate.html";
+
+        } catch (error) {
+            console.error("Erreur:", error);
+            ErrorElement.innerText="Erreur interne";
+        }
+    });
+});
